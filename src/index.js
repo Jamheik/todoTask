81f35ac3010b5896d8dd5b8d74c744e384ac8153
Jamheik <heikkinenjami03@gmail.com>
2024-11-05 02:19:38 +0200

    pt 7 done

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom/client";
+import Home from "./home";
+import Authentication, {
+  AuthenticationMode,
+} from "./components/Authentication";
+import ErrorPage from "./ErrorPage";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import UserProvider from "./context/UserProvider";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/signin",
+    element: <Authentication authenticationMode={AuthenticationMode.Login} />,
+  },
+  {
+    path: "/signup",
+    element: (
+      <Authentication authenticationMode={AuthenticationMode.Register} />
+    ),
+  },
+  {
+    element: <ProtectedRoute />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <UserProvider>
+      <RouterProvider router={router}/>
+    </UserProvider>
   </React.StrictMode>
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
